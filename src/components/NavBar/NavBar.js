485c9744f98cb8c737e7bbb8de247bc6ffc68e6d
juandelellis <juandelellis@gmail.com>
2021-01-10 12:33:16 -0300

    update navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+export const NavBar = () => {
+    return (
+        <div className='containerNav'>
+            <div className='containerNav'>
+                LOGO
+            </div>
+            <p>Home</p>
+            <p>Categorias</p>
+            <p>Carrito</p>
+        </div>
+    )
+}

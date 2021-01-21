3f39b44d1ee3bf3613e1d40b6327fe54d293f7fd
juandelellis <juandelellis@gmail.com>
2021-01-20 22:35:18 -0300

    update react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import { NavBar } from './components/NavBar/NavBar'
 import { ItemListContainer} from './components/ItemListContainer/ItemListContainer'
 import CountContainer from './components/CountContainer/CountContainer'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <NavBar />
+      <Switch>
+      <Route path="/contact">
+      <Contact />
+      </Route>
+      <Route path="/">
       <ItemListContainer />
       <CountContainer initial={1} stock={5} />
+      </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React  from "react";
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>
+                Esta es la página de contacto
+            </h2>
+            <Link to="/">Go Back </Link>
+        </div>
+    );
+};
+
+export default Contact

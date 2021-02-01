9c1a7f8518a04da4c4ac8687506c1f1bfc5a6307
juandelellis <juandelellis@gmail.com>
2021-02-01 19:48:50 -0300

    Clase 9

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import "./ItemDetail.css"
 import Item from "../Item/Item";
+import Count from "../Count"
+import { useParams } from "react-router-dom";
 export const ItemDetail = ({items} ) => {
+    const { itemId } = useParams();
     return (
         <React.Fragment>
              <div>Conocé este producto</div>
              <ul>
              {items && items.map (item => <Item key={item.id} item={item.title}  />)} 
              </ul>
+             <Count item={items}/>
+             <p>{items.stock > 0 ? `Stock disponible: ${items.stock}` : `No hay Stock disponible de este producto`}</p>
+            <button>Terminá tu compra</button>
         </React.Fragment>
+        
     )
 }
 export default ItemDetail;  
